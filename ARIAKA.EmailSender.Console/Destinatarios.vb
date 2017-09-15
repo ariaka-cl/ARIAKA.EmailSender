519a9e50c03970caf320b4f93ae1e464fc3a83ff
jtabilo <btabilosk8@gmail.com>
2017-09-15 00:48:15 -0400

    commit inicial
    Se inicia el proyecto

---
+++ ARIAKA.EmailSender.Console/Destinatarios.vb
@@ -0,0 +1,11 @@
+﻿Module Destinatarios
+
+    Sub Main()
+        Dim ss As New SenderEmail
+        Dim listEmail As New List(Of Model.Destinatario)
+        listEmail.Add(New Model.Destinatario With {.Email = "jtabilot@gmail.com"})
+        listEmail.Add(New Model.Destinatario With {.Email = "diego.bonilla.vargas@gmail.com"})
+        ss.SendEmail(listEmail)
+    End Sub
+
+End Module
---
+++ ARIAKA.EmailSender.Console/Model/Destinatario.vb
@@ -0,0 +1,7 @@
+﻿Namespace Model
+    Public Class Destinatario
+        Public Property Nombre As String
+        Public Property Email As String
+        Public Property Run As String
+    End Class
+End Namespace
